25355e5272ef7986ae290fea8d96b5444ff1e19c
Rufsan Hossain Santo <rufsanhossain315@gmail.com>
2022-06-11 19:44:55 +0600

    import libraries

--- titanic-survivor.py
+++ titanic-survivor.py
@@ -1,9 +1,17 @@
 # importing libraries
 
-import pandas as pd # data processing, csv file I/O
-import numpy as np # linear algebra
+import pandas as pd  # data processing, csv file I/O
+import numpy as np  # linear algebra
 
 # visualization libraries
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# data preparation, model building and accuracy checking libraries
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+
+import os  # for creating and removing submission files
