f965b225c11ba5d930689a298516fd74f26ac19b
Rufsan Hossain Santo <rufsanhossain315@gmail.com>
2022-06-11 19:36:08 +0600

    first commit

---
+++ titanic-survivor.py
@@ -0,0 +1,9 @@
+# importing libraries
+
+import pandas as pd # data processing, csv file I/O
+import numpy as np # linear algebra
+
+# visualization libraries
+
+import matplotlib.pyplot as plt
+import seaborn as sns
